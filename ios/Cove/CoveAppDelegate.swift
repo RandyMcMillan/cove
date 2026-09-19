7d370b5e2a45c2c5df64b214d15c4f4981689581
randymcmillan <randymcmillan@protonmail.com>
2026-09-19 17:14:16 -0400

    macos: fix Help menu via Info.plist CFBundleHelpBookName + CoveHelp/index.html

--- ios/Cove/CoveAppDelegate.swift
+++ ios/Cove/CoveAppDelegate.swift
@@ -20,34 +20,4 @@ final class CoveAppDelegate: NSObject, UIApplicationDelegate {
         configuration.delegateClass = CovePopupSceneDelegate.self
         return configuration
     }
-
-    @available(iOS 16.0, *)
-    override func buildMenu(with builder: UIMenuBuilder) {
-        super.buildMenu(with: builder)
-
-        guard builder.system == .main else { return }
-
-        let helpCommand = UICommand(
-            title: "Cove Help",
-            action: #selector(openHelp),
-            input: "?",
-            modifierFlags: .command
-        )
-
-        let helpMenu = UIMenu(
-            title: "Help",
-            image: nil,
-            identifier: .help,
-            options: .displayAsInline,
-            children: [helpCommand]
-        )
-
-        builder.insertSibling(helpMenu, afterMenu: .about)
-    }
-
-    @objc private func openHelp() {
-        if let url = URL(string: "https://covebitcoinwallet.com/support") {
-            UIApplication.shared.open(url)
-        }
-    }
 }
